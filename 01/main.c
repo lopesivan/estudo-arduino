90893c2c89f301730f87f067bb3db053d5c4577a
Ivan Lopes <lopesivan.ufrj@gmail.com>
2024-05-27 18:19:45 -0300

    Atualiza o arquivo 01/main.c

--- 01/main.c
+++ 01/main.c
@@ -29,22 +29,91 @@
 // | XTAL2       | PB7 |     |     |            |
 // | RESET       |     | PC6 |     |            |
 
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+#define NULL 0
 
-#define BV(bit)              (1 << bit)
-#define setBit(byte, bit)    (byte |= BV(bit))
-#define clearBit(byte, bit)  (byte &= ~BV(bit))
-#define toggleBit(byte, bit) (byte ^= BV(bit))
+enum _bool
+{
+    false = 0,
+    true  = 1
+};
+
+typedef enum _bool bool;
+
+typedef volatile uint8_t* address;
+
+#define PORTX(led, X, num) do { \
+    led.address = &DDR##X;      \
+    led.id_addr = DD##X##num;   \
+    led.port    = &PORT##X;     \
+    led.id_port = PORT##X##num; \
+} while(0)
+
+struct gpio_t
+{
+    address  address;
+    uint8_t  id_addr;
+    address  port;
+    uint8_t  id_port;
+};
+
+typedef struct gpio_t gpio;
+
+
+int output   (address port, uint8_t mask);
+int turn_on  (address port, uint8_t mask);
+int turn_off (address port, uint8_t mask);
+
+typedef int (*function) (address, uint8_t);
+
+enum
+{
+    BLINK_DELAY_MS = 1000,
+};
 
 int main (void)
 {
-    DDRB = BV (5);
-    setBit (PORTB, 5);
-    //            543210
-    //DDRB  = 0b00100000;  // defino PB5 como saida
-    //PORTB |= (1<<5);     // aplicamos 5V no pino PB5
+    function f = NULL;
 
-    while (1)
-        _delay_ms (2000);
+    gpio fonte;
+
+    PORTX (fonte, B, 5);
+
+    f = output;
+    f (fonte.address, fonte.id_addr);
+
+    bool finished = false;
+
+    f = turn_on;
+    f (fonte.port, fonte.id_port);
+
+    while (!finished)
+    {
+        _delay_ms (BLINK_DELAY_MS);
+    }
+
+    return EXIT_SUCCESS;
+}
+
+
+int output (address port, uint8_t mask)
+{
+    *port |= _BV (mask);
+
+    return 0;
+}
+
+int turn_on (address port, uint8_t mask)
+{
+    output (port, mask);
+
+    return 0;
+}
+
+int turn_off (address port, uint8_t mask)
+{
+    *port &= ~ _BV (mask);
 
     return 0;
 }
