af8977bde94703dc23798f92e930c62b2d1ebbb8
Ivan Lopes <lopesivan.ufrj@gmail.com>
2024-05-27 17:17:19 -0300

    Atualiza o arquivo 04/main.c

--- 04/main.c
+++ 04/main.c
@@ -1,7 +1,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-// -> Quero o pino 13 com 5V
+// -> Quero o pino 7 com 5V
 
 // | Digital Pin | PB# | PC# | PD# | Analog Pin |
 // | ----------- | --- | --- | --- | ---------- |
@@ -23,23 +23,18 @@
 // | A1          |     | PC1 |     | A1         |
 // | A2          |     | PC2 |     | A2         |
 // | A3          |     | PC3 |     | A3         |
-// | A4          |     | PC4 |     | A4         |
+//*|*A4**********|*****|*PC4*|*****|*A4*********|
 // | A5          |     | PC5 |     | A5         |
 // | XTAL1       | PB6 |     |     |            |
 // | XTAL2       | PB7 |     |     |            |
 // | RESET       |     | PC6 |     |            |
 
 
-#define BV(bit)              (1 << bit)
-#define setBit(byte, bit)    (byte |= BV(bit))
-#define clearBit(byte, bit)  (byte &= ~BV(bit))
-#define toggleBit(byte, bit) (byte ^= BV(bit))
-
 int main (void)
 {
     //          543210
-    DDRB  = 0b00100000;  // defino PB5 como saida
-    PORTB |= (1<<5);     // aplicamos 5V no pino PB5
+    DDRC  = 0b00010000;  // defino PB5 como saida
+    PORTC |= (1<<4);     // aplicamos 5V no pino PB5
 
     while (1)
         _delay_ms (2000);
