1f8b49934e354138af3b88af201650b084768369
Ivan Lopes <lopesivan.ufrj@gmail.com>
2024-06-05 08:43:01 -0300

    Remove o arquivo 13/12/.gitignore

--- 13/main.c
+++ 13/main.c
@@ -2,8 +2,9 @@
 #include <avr/interrupt.h>
 
 #define ADC_PRESCALER 128
+#define BAUD 9600
+#define MYUBRR F_CPU/16/BAUD-1
 #define TIMER1_PRESCALER 64
-#define ZERO_CROSS_THRESHOLD 0.1
 #define REF 2.5
 
 #define setBit(byte, bit)    (byte |= _BV(bit))
@@ -15,7 +16,48 @@ volatile float voltage;
 volatile float ramp = 0.0;
 volatile float ramp1 = 0.0;
 volatile float ramp2 = 0.0;
-volatile float delta = 5.0 / (60.0 * 1);
+volatile float delta = .5;
+
+void usart_init (unsigned int ubrr)
+{
+    // Configura a taxa de transmissão
+    UBRR0H = (unsigned char) (ubrr >> 8);
+    UBRR0L = (unsigned char)ubrr;
+    // Habilita o transmissor
+    UCSR0B = (1 << TXEN0);
+    // Define o formato da trama: 8 bits de dados, 1 bit de parada
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+}
+
+void usart_transmit (unsigned char data)
+{
+    // Aguarda até o buffer de transmissão estar vazio
+    while (! (UCSR0A & (1 << UDRE0)));
+    // Envia o dado
+    UDR0 = data;
+}
+
+void usart_print (const char* str)
+{
+    while (*str)
+    {
+        usart_transmit (*str++);
+    }
+}
+
+void usart_println (const char* str)
+{
+    usart_print (str);
+    usart_transmit ('\r');
+    usart_transmit ('\n');
+}
+
+void usart_print_float (float value)
+{
+    char buffer[20];
+    dtostrf (value, 6, 2, buffer);
+    usart_print (buffer);
+}
 
 void adc_init (void)
 {
@@ -45,7 +87,7 @@ void timer1_init (void)
 {
     // Configura o Timer1 para CTC mode
     TCCR1B |= (1 << WGM12); // Modo CTC (Clear Timer on Compare Match)
-    OCR1A = (F_CPU / TIMER1_PRESCALER / 1) - 1; // Configura para 1 kHz
+    OCR1A = (F_CPU / TIMER1_PRESCALER / .0167) - 1; // Configura para 1 kHz
     TIMSK1 |= (1 << OCIE1A); // Habilita interrupção de comparação A
     TCCR1B |= (1 << CS11) | (1 << CS10); // Prescaler de 64
 }
@@ -57,17 +99,11 @@ ISR (TIMER1_COMPA_vect)
 
     if (voltage >= 1.82)
     {
-        ramp1 += delta;
-        ramp2 = 0;
-    }
-    else
-    {
-        ramp2 += delta;
-        ramp1 = 0;
+        ramp += delta;
     }
 
-    ramp = ramp1+ramp2;
-
+    usart_print_float (ramp);
+    usart_println ("");
     if (ramp >5 )
         ramp = 0;
 
@@ -87,6 +123,7 @@ int main (void)
 {
     adc_init();
     timer1_init();
+    usart_init (MYUBRR);
     sei(); // Habilita interrupções globais
 
     // Inicializa o pino PB5 como saída
